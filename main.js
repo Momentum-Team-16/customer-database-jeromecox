d7226248513a0fe77c21fc78f4804d4090a33928
jeromecox <jeromecox@hotmail.com>
2022-11-16 09:21:23 -0600

    all items centered, email url needs work

--- main.js
+++ main.js
@@ -10,18 +10,18 @@ function dateConvert(date) {
 for (let customer of customers) {
   let card = document.createElement("div");
   card.classList.add("card");
-  card.classList.add("large");
+  card.classList.add("small");
   card.classList.add("col");
   card.classList.add("s12");
   card.classList.add("m9");
   card.classList.add("l4");
-  // create a card for the customer
+  // created a card for the customer
 
   let thumb = document.createElement("div");
   thumb.classList.add("card-image");
   thumb.classList.add("circle");
   let pic = document.createElement("img");
-  pic.src = customer.picture.thumbnail;
+  pic.src = customer.picture.large;
 
   thumb.appendChild(pic);
   card.appendChild(thumb);
@@ -37,12 +37,19 @@ for (let customer of customers) {
   // add the name to the card
   card.appendChild(name);
 
-  // let content = document.createElement("div");
-  // content.classList.add("card-content");
+  let content = document.createElement("div");
+  content.classList.add("card-content");
+  content.classList.add("medium");
 
-  let email = document.createElement("div");
-  email.innerText = `${customer.email}`;
+  let email = document.createElement("a");
+  let emailText = document.createTextNode(`${customer.email}`);
+  email.appendChild(emailText);
+  email.href = `${emailText}`;
   card.appendChild(email);
+
+  // let email = document.createElement("div");
+  // email.innerText = `${customer.email}`;
+  // content.appendChild(email);
 
   let address = document.createElement("div");
   address.innerText = `${customer.location.street.number} ${
@@ -51,19 +58,19 @@ for (let customer of customers) {
   ${customer.location.city}, ${nameToAbbr(customer.location.state)} ${
     customer.location.postcode
   }`;
-  card.appendChild(address);
+  content.appendChild(address);
 
   let birthday = document.createElement("div");
   let date1 = dateConvert(customer.dob.date);
   birthday.innerText = `DOB: ${date1}`;
-  card.appendChild(birthday);
+  content.appendChild(birthday);
 
   let custDate = document.createElement("div");
   let date2 = dateConvert(customer.registered.date);
   custDate.innerText = `Customer since: ${date2}`;
-  card.appendChild(custDate);
+  content.appendChild(custDate);
 
-  // card.appendChild(content);
+  card.appendChild(content);
 
   // add card to customerContainer
   customerContainer.appendChild(card);
