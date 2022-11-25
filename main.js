0e65dae862f00ea2174ad179bb0fb386531542b2
jeromecox <jeromecox@hotmail.com>
2022-11-25 14:46:00 -0600

    create element helper function added

--- main.js
+++ main.js
@@ -5,70 +5,54 @@ const customerContainer = document.querySelector("#customers");
 function dateConvert(date) {
   return moment(date).format("ll");
 }
-
+// created a new element and adds class(es) to the element
+function divClass(type, classArray, parent) {
+  let newElement = document.createElement(type);
+  newElement.classList.add(...classArray);
+  parent.appendChild(newElement);
+  return newElement;
+}
 // loop through the customer objects and build html for each one
 for (let customer of customers) {
-  let card = document.createElement("div");
-  card.classList.add("card");
-  card.classList.add("small");
-  card.classList.add("col");
-  card.classList.add("s12");
-  card.classList.add("m9");
-  card.classList.add("l4");
+  let custCard = divClass(
+    "div",
+    ["card", "small", "col", "s12", "m9", "l4", "white"],
+    customerContainer
+  );
   // created a card for the customer
 
-  let thumb = document.createElement("div");
-  thumb.classList.add("card-image");
-  thumb.classList.add("circle");
-  let pic = document.createElement("img");
+  let thumb = divClass("div", ["card-image", "circle"], custCard);
+  let pic = divClass("img", "", thumb);
   pic.src = customer.picture.large;
 
-  thumb.appendChild(pic);
-  card.appendChild(thumb);
-
   // create a div for name and apply the materialize class to it
-  let name = document.createElement("div");
-  name.classList.add("card-title");
+  let name = divClass("div", ["card-title", "white"], custCard);
   let firstName =
     customer.name.first[0].toUpperCase() + customer.name.first.slice(1);
   let lastName =
     customer.name.last[0].toUpperCase() + customer.name.last.slice(1);
   name.innerText = `${firstName} ${lastName}`;
-  // add the name to the card
-  card.appendChild(name);
 
-  let content = document.createElement("div");
-  content.classList.add("card-content");
-  content.classList.add("medium");
-
-  let email = document.createElement("a");
-  email.classList.add("emailLink");
+  let email = divClass("a", ["emailLink", "grey-text"], custCard);
   let emailText = document.createTextNode(`${customer.email}`);
   email.appendChild(emailText);
   email.href = `mailto:${customer.email}`;
-  card.appendChild(email);
 
-  let address = document.createElement("div");
+  let content = divClass("div", ["card-content", "white", "medium"], custCard);
+
+  let address = divClass("div", ["white"], content);
   address.innerText = `${
     customer.location.street.number
   } ${customer.location.street.name.trim()}
   ${customer.location.city}, ${nameToAbbr(customer.location.state)} ${
     customer.location.postcode
   }`;
-  content.appendChild(address);
 
-  let birthday = document.createElement("div");
+  let birthday = divClass("div", ["white"], content);
   let date1 = dateConvert(customer.dob.date);
   birthday.innerText = `DOB: ${date1}`;
-  content.appendChild(birthday);
 
-  let custDate = document.createElement("div");
+  let custDate = divClass("div", ["white"], content);
   let date2 = dateConvert(customer.registered.date);
   custDate.innerText = `Customer since: ${date2}`;
-  content.appendChild(custDate);
-
-  card.appendChild(content);
-
-  // add card to customerContainer
-  customerContainer.appendChild(card);
 }
